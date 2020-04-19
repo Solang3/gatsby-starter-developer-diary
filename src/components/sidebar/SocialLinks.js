902bfcfa775c4cfded5b165e0d45f8113c719772
Solange Gonzalez Ferrero <solangegf@gmail.com>
2020-04-19 10:55:34 +0200

    Update Social links size

--- src/components/sidebar/SocialLinks.js
+++ src/components/sidebar/SocialLinks.js
@@ -15,31 +15,31 @@ const SocialLinks = ({ contacts }) => {
             <a className="text-secondary p-2"
                 href={contacts.linkedin}>
                 <span title="Linked In">
-                    <FaLinkedin size={26} style={{ color: "secondary" }} />
+                    <FaLinkedin size={24} style={{ color: "secondary" }} />
                 </span>
             </a>
             <a className="text-secondary p-2"
                 href={contacts.github}>
                 <span title="GitHub">
-                    <FaGithubSquare size={26} style={{ color: "secondary" }} />
+                    <FaGithubSquare size={24} style={{ color: "secondary" }} />
                 </span>
             </a>
             <a className="text-secondary p-2"
                 href={contacts.stackoverflow}>
                 <span title="Stack Overflow">
-                    <FaStackOverflow size={26} style={{ color: "secondary" }} />
+                    <FaStackOverflow size={24} style={{ color: "secondary" }} />
                 </span>
             </a>
             <a className="text-secondary p-2"
                 href={contacts.freecodecamp}>
                 <span title="freeCodeCamp">
-                    <FaFreeCodeCamp size={26} style={{ color: "secondary" }} />
+                    <FaFreeCodeCamp size={24} style={{ color: "secondary" }} />
                 </span>
             </a>
             <a className="text-secondary p-2"
                 href={contacts.twitter}>
                 <span title="Twitter">
-                    <FaTwitterSquare size={26} style={{ color: "secondary" }} />
+                    <FaTwitterSquare size={24} style={{ color: "secondary" }} />
                 </span>
             </a>
         </div>
